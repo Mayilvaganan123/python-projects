868984536f2e6e140e3f8f27948a314142f82081
Mayilvaganan i <132040229+Mayilvaganan123@users.noreply.github.com>
2025-11-20 00:18:03 +0530

    Delete menu directory

--- menu/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import FoodItems
-
-# Register your models here.
-@admin.register(FoodItems)
-class Fooditemadmin(admin.ModelAdmin):
-    list_display =('name','price','category')
-    search_fields = ('name','category')
--- menu/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('submit/', views.submit_order, name='submit_order'),
-    path('dashboard/',views.Dashboard, name='Dashboard'),
-    path('', views.menu_view, name='menu'),
-]
